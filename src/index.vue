3c4e8488a49e8864de5351a8288d45fc8fe83806
temaput <putilkin@gmail.com>
2023-11-30 14:23:40 +0000

    Prepare for xml

---
+++ src/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <App v-if="path==='/'"/>
+  <Books v-else-if="path==='/books'"/>
+  
+</template>
+<script setup>
+  import {computed} from "vue";
+  import App from "./App.vue"
+  import Books from "./pages/Books.vue"
+
+  const path = computed(() => document.location.pathname)
+
+  console.log("PAth is: ", path.value)
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from './index.vue';
 import './assets/base.css'
 
 const components = import.meta.globEager('./components/*.vue');
